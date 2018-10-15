f52780162559cf9dfdd7476671dc121354567ec4
Steven128 <2678061071@qq.com>
2018-10-15 15:32:02 +0800

    2018-10-15

--- fin_mgment/orderList/index.php
+++ fin_mgment/orderList/index.php
@@ -1,3 +1,8 @@
+<!-- /fin_mgment/orderList -->
+
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -6,19 +11,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>财务管理-餐饮店管理系统</title>
-    <link type="text/css" rel="stylesheet" href="../css/bootstrap.css" />
-    <link type="text/css" rel="stylesheet" href="../css/iconfont.css" />
-    <link type="text/css" rel="stylesheet" href="../css/page.css" />
-    <link type="text/css" rel="stylesheet" href="../css/sidebar-menu.css" />
+    <link type="text/css" rel="stylesheet" href="../../css/bootstrap.css" />
+    <link type="text/css" rel="stylesheet" href="../../css/iconfont.css" />
+    <link type="text/css" rel="stylesheet" href="../../css/page.css" />
+    <link type="text/css" rel="stylesheet" href="../../css/sidebar-menu.css" />
+    <link type="text/css" rel="stylesheet" href="../../css/tablesorter.css" />
 
-    <script type="text/javascript" src="../js/jQuery/jquery-1.11.3.min.js"></script>
-    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="../js/page.js"></script>
-    <script type="text/javascript" src="../js/xcConfirm.js"></script>
-    <script type="text/javascript" src="../js/jquery.pjax.js"></script>
+    <script type="text/javascript" src="../../js/jQuery/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="../../js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../../js/page.js"></script>
+    <script type="text/javascript" src="../../js/xcConfirm.js"></script>
+    <script type="text/javascript" src="../../js/plugins/jquery.tablesorter.min.js"></script>
+    <script type="text/javascript" src="../../js/plugins/jquery.filtertable.js"></script>
+    <script type="text/javascript" src="../../js/jquery.pjax.js"></script>
+    <?php
+    if (!isset($_SESSION['admin_id'])) {
+        echo "<script>$(document).ready(() => {window.location.replace(\"../../login\");});</script>";
+    } elseif ($_SESSION['admin_type'] != 3) {
+        echo "<script>$(document).ready(() => {window.location.replace(\"../../dashboard\");});</script>";
+    }
+    ?>
 </head>
 
 <body>
+    <?php
+    $conn = oci_connect('scott', '123456', 'localhost:1521/ORCL', "AL32UTF8"); //连接oracle数据库
+    ?>
     <div class="container">
         <header class="head-content">
             <div class="site-branding">
@@ -28,7 +46,7 @@
                     </div>
                 </a>
                 <div class="site-title">
-                    <a herf="../dashboard" rel="home">餐饮店管理系统</a>
+                    <a herf="../../dashboard" rel="home">餐饮店管理系统</a>
                     <h5>财务管理系统</h5>
                 </div>
             </div>
@@ -37,14 +55,23 @@
             <div class="bar-box">
                 <aside class="left-bar">
                     <div class="admin-box">
-                        <img class="userPic" />
-                        <h4 class="online-user"></h4>
-                        <i class="iconfont icon-certificated" style="color: #1afa29;"></i>
-                        <h5 class="user-type"></h5>
+                        <?php
+                        $admin_type = $_SESSION['admin_type'];
+                        if ($admin_type == 1) {
+                            $admin_type = "超级管理员";
+                        } elseif ($admin_type == 2) {
+                            $admin_type = "管理员";
+                        } elseif ($admin_type == 3) {
+                            $admin_type = "财务管理";
+                        } elseif ($admin_type == 4) {
+                            $admin_type = "库存管理";
+                        }
+                        echo "<img class=\"userPic\" src=\"" . $_SESSION['admin_pic'] . "?" . mt_rand(10000, 99999) . "\" /><h4 class=\"online-user\">" . $_SESSION['admin_name'] . "</h4><i class=\"iconfont icon-certificated\" style=\"color: #1afa29;\"></i><h5 class=\"user-type\">" . $admin_type . "</h5>";
+                        ?>
                     </div>
                     <section class="sidebar">
                         <ul class="sidebar-menu">
-                            <li class="treeview active">
+                            <li class="treeview">
                                 <a href="javascript:void(0);">
                                     <i class="iconfont icon-overview"></i>
                                     <span>总览</span>
@@ -52,9 +79,9 @@
                                         <i class="iconfont icon-down-arrow" style="font-size:12px;"></i>
                                     </span>
                                 </a>
-                                <ul class="treeview-menu menu-open">
+                                <ul class="treeview-menu">
                                     <li>
-                                        <a id="menu-finOverview-item" href="javascript:void(0);" class="innerActive">
+                                        <a id="menu-finOverview-item" href="javascript:void(0);">
                                             <i class="iconfont icon-list"></i>财务总览</a>
                                     </li>
                                 </ul>
@@ -82,7 +109,7 @@
                                     </li>
                                 </ul>
                             </li>
-                            <li class="treeview">
+                            <li class="treeview active">
                                 <a href="javascript:void(0);">
                                     <i class="iconfont icon-sale"></i>
                                     <span>销售管理</span>
@@ -90,9 +117,9 @@
                                         <i class="iconfont icon-down-arrow" style="font-size:12px;"></i>
                                     </span>
                                 </a>
-                                <ul class="treeview-menu">
+                                <ul class="treeview-menu" menu-open>
                                     <li>
-                                        <a id="menu-orderList-item" href="javascript:void(0);">
+                                        <a id="menu-orderList-item" href="javascript:void(0);" class="innerActive">
                                             <i class="iconfont icon-order"></i>查看今日订单</a>
                                     </li>
                                     <li>
@@ -118,7 +145,7 @@
                             </li>
                         </ul>
                     </section>
-                    <script src="../js/sidebar-menu.js"></script>
+                    <script src="../../js/sidebar-menu.js"></script>
                     <script>
                         $.sidebarMenu($('.sidebar-menu'))
                     </script>
@@ -126,34 +153,65 @@
                 <div class="mask"></div>
                 <div class="main-bar">
                     <div class="title">
-                        <h4 class="title-left">更新员工信息</h4>
+                        <h4 class="title-left">查看今日订单</h4>
                     </div>
                     <div class="box-wrap">
-
+                        <div class="box">
+                            <div class="inner-top-wrap"></div>
+                            <div class="inner-box">
+                                <!-- <table class="financeListTable tablesorter result">
+                                    <thead>
+                                        <tr>
+                                            <th>序号</th>
+                                            <th>日期</th>
+                                            <th>营业额</th>
+                                            <th>花销</th>
+                                            <th>盈利</th>
+                                            <th>操作</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="financeListTableBody">
+                                        <?php
+                                        $sql_query = "SELECT FINANCE_ID,FIN_DATE,MONTH,TURNOVER,COST,PROFIT FROM SCOTT.FINANCE ORDER BY FIN_DATE DESC";
+                                        $statement = oci_parse($conn, $sql_query);
+                                        oci_execute($statement);
+                                        $count = 0;
+                                        while ($row = oci_fetch_array($statement, OCI_RETURN_NULLS)) { //查询结果集
+                                            $count++;
+                                            echo "<tr><td>$count</td><td>$row[1]</td><td>$row[3]</td><td>$row[4]</td><td>$row[5]</td><td><a class=\"table-update-btn update-finance\" href = \"javascript:void(0);\" onclick=\"update_finance('" . $row[0] . "')\"><i class=\"iconfont icon-update\"></i></a></td></tr>";
+                                        }
+                                        ?>
+                                <script>
+                                    $(() => {
+                                        $(".financeListTable").tablesorter();
+                                    });
+                                    $(() => {
+                                        $(".financeListTable").filterTable();
+                                    });
+                                </script>
+                                </tbody>
+                                </table> -->
+                            </div>
+                        </div>
                     </div>
                     <script>
                         $(document).ready(() => {
-                            window.location.replace("finOverview");
-
                             function changeMainBar(itemName) {
                                 $("#menu-" + itemName + "-item").click(() => {
                                     $.pjax({
-                                        url: "" + itemName,
+                                        url: "../" + itemName,
                                         container: 'html'
                                     });
                                 });
                             }
                             //
-                            changeMainBar("finOverview");
                             changeMainBar("overheadList");
                             changeMainBar("overheadHistory");
                             changeMainBar("addOverhead");
-                            changeMainBar("orderList");
                             changeMainBar("orderHistory");
                             changeMainBar("salaryList");
                         });
                     </script>
-                    <div class="shelter" onclick="hideBox()" style="background-color: rgba(0,0,0,0.5);z-index: 100;"></div>
                 </div>
             </div>
         </div>
---
+++ js/fin_mgment/fin.add.validate.js
@@ -0,0 +1,29 @@
+$(document).ready(() => {
+    /**
+     * 获取登录信息
+     */
+    $.ajax({
+        type: "GET",
+        url: "../../php/check_login.php?request=check",
+        dataType: "JSON",
+        success: (e) => {
+            $(".userPic").attr('src', e.admin_pic + "?" + num);
+            $(".online-user").html(e.admin_name);
+            admin_type = e.admin_type;
+            if (admin_type == 1) {
+                admin_type = "超级管理员";
+            } else if (admin_type == 2) {
+                admin_type = "管理员";
+            } else if (admin_type == 3) {
+                admin_type = "财务管理";
+            } else if (admin_type == 4) {
+                admin_type = "仓库管理";
+            }
+            $(".user-type").html(admin_type);
+        },
+        error: (err) => {
+            console.log(err)
+        }
+    })
+
+})
