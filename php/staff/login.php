2033362a434dcdfcd08b3e27e8ae566c864daf18
Giaooo <1318513681@qq.com>
2018-10-23 20:11:01 +0800

    2018-10-23-20-10

--- php/staff/login.php
+++ php/staff/login.php
@@ -1,10 +1,8 @@
 <?php
 
-if (isset($_POST['request']) && $_POST['request'] != "") {
-    $request = $_POST['request'];
-} else {
-    die();
-}
+$request = $_POST;
+// json_decode($request);
+var_dump($request);
 
 $conn = oci_connect('scott', '123456', 'localhost:1521/ORCL', "AL32UTF8"); //连接oracle数据库
 if (!$conn) { //未连接成功，终止脚本并返回错误信息
